8677920539e2bb198b1df7487908cb30e580a042
kayesokua <kaye.sokua@code.berlin>
2023-03-06 16:54:27 +0100

    including instance and migrations for illustration, no sensitive data in it

--- application/db.py
+++ application/db.py
@@ -7,6 +7,20 @@ from flask import g
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
 
+
+import os
+import urllib.parse as up
+import psycopg2
+
+up.uses_netloc.append("postgres")
+url = up.urlparse(os.environ["DATABASE_URL"])
+conn = psycopg2.connect(database=url.path[1:],
+user=url.username,
+password=url.password,
+host=url.hostname,
+port=url.port
+)
+
 def get_db():
     if "db" not in g:
         g.db = sqlite3.connect(
