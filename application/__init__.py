de08238adcf159cc31e289195b6d7d6a4da51083
kayesokua <kaye.sokua@code.berlin>
2023-03-27 12:39:09 +0200

    extract database module from main application file

--- application/__init__.py
+++ application/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 from config import Config
-
-db = SQLAlchemy()
+from application.extensions.db import db, migrate
 
 def create_app(config_class=Config):
+
     app = Flask(__name__)
     app.config.from_object(config_class)
     db.init_app(app)
 
     with app.app_context():
+        from application.models import store, auth
         db.create_all()
+
+        migrate.init_app(app, db, compare_type=True)
 
     from application.views import auth, store
     app.register_blueprint(auth.bp)
