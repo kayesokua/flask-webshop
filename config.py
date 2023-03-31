ed9c48675898f9ef2dd0aecacddc0c905e337d12
kayesokua <kaye.sokua@code.berlin>
2023-03-31 20:34:25 +0200

    Separate config classes for development, testing, and production environments with different database URI configurations.

--- config.py
+++ config.py
@@ -3,13 +3,20 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+class TestingConfig(Config):
+    DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI_TEST')
+
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
