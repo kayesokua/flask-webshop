31d0b51cd7f479e7c8a903a977de20e90da79c4d
kayesokua <kaye.sokua@code.berlin>
2023-04-13 10:40:11 +0200

    Added timestamp to user model

--- application/models/products.py
+++ application/models/products.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from application import db
 
 class Product(db.Model):
@@ -11,4 +12,16 @@ class Product(db.Model):
     stock = Column(Integer, nullable=False, default=0)
     description = Column(String, nullable=False)
     image = Column(String, nullable=False)
+    stripe_product_ref = Column(String, nullable=True)
+    price_id = Column(Integer, ForeignKey('prices.id'), nullable=True)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
     admin = relationship('User', backref='products')
+
+class Prices(db.Model):
+    __tablename__ = 'prices'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    product_id = Column(Integer, ForeignKey('products.id'), nullable=True)
+    price = Column(Float, nullable=True)
+    stripe_price_id = Column(String, nullable=True)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
