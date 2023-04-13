2b21a526a5687763ba7825621493a6ce055fdbd6
kayesokua <kaye.sokua@code.berlin>
2023-04-13 10:40:33 +0200

    removed test cli command

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,6 @@ import click
 
 app = create_app()
 
-
 @click.command("create-admin")
 @click.argument("username")
 @click.argument("password")
@@ -12,11 +11,4 @@ def create_admin_command(username, password):
     with app.app_context():
         create_admin(username, password)
 
-
-@click.command("test")
-def test_command():
-    print("test")
-
-
 app.cli.add_command(create_admin_command)
-app.cli.add_command(test_command)
