7336b63fa968d8595bca3050e2f0aabcdc370863
kayesokua <kaye.sokua@code.berlin>
2023-04-17 00:08:23 +0200

    Implement verification process on checkout

--- application/models/orders.py
+++ application/models/orders.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
@@ -12,17 +12,17 @@ class Orders(db.Model):
     buyer_id = Column(UUID, ForeignKey('users.id'), nullable=False)
     shipping_fee = Column(Float, nullable=False)
     grand_total = Column(Float, nullable=False)
-
     delivery_status = Column(String(10), default='pending', nullable=True)
     delivery_tracking_url = Column(String(350), nullable=True)
-
     payment_status = Column(String(10), default='pending', nullable=True)
     stripe_payment_id = Column(String(255), nullable=True)
     stripe_payment_url = Column(String(350), nullable=True)
-
+    checkout_verification_code = Column(String(6), nullable=True)
+    checkout_verification_attemps = Column(Integer, default=0)
+    is_purchase_verified = Column(Boolean, default=False)
+    is_expired = Column(Boolean, default=False)
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
-
     buyer = relationship('User', backref='orders')
     address = relationship('DeliveryAddress', foreign_keys=[address_id], backref='orders')
     order_lines = relationship('OrderLine', backref='order')
--- application/orders/routes.py
+++ application/orders/routes.py
@@ -1,12 +1,14 @@
-import os, stripe
+import os, stripe, pytz
 from sqlalchemy import and_
-from flask import Blueprint, flash, redirect, render_template, url_for, session
+from flask import Blueprint, flash, redirect, render_template, url_for, session, request
 from flask_login import login_required, current_user
 from application.extensions.db import db
 from application.extensions.cache import cache
 from application.extensions.limiter import limiter
-from application.models import Product, Orders, OrderLine, Prices
-from application.orders.forms import CheckoutForm
+from application.extensions.twilio import generate_custom_code, send_checkout_verification
+from application.models import Product, Orders, OrderLine, Prices, DeliveryAddress
+from application.orders.forms import CheckoutForm, VerifyOrderForm
+from datetime import timedelta, datetime
 
 bp = Blueprint("orders", __name__)
 
@@ -16,7 +18,6 @@ def handle_cart():
     shipping_fee = 0
     index = 0
     total_charge = 0
-
     for item in session['cart']:
         product = Product.query.filter_by(id=item['id']).first()
         if product is not None:
@@ -46,21 +47,23 @@ def handle_cart():
 @bp.route('/cart/add/<uuid:id>')
 @login_required
 def cart_add_item(id):
-    id = int(id)
     if 'cart' in session:
         cart = session['cart']
         item_exists = False
         for item in cart:
-            if item['id'] == id:
+            if item['id'] == str(id):
                 item['quantity'] += 1
                 item_exists = True
                 break
         if not item_exists:
-            cart.append({'id': id, 'quantity': 1})
+            cart.append({'id': str(id), 'quantity': 1})
         session['cart'] = cart
     else:
-        session['cart'] = [{'id': id, 'quantity': 1}]
+        session['cart'] = [{'id': str(id), 'quantity': 1}]
     session.modified = True
+    cart_items = [{'id': item['id'], 'quantity': item['quantity']} for item in session['cart']]
+    print("testing cart items..")
+    print(cart_items)
     return redirect(url_for('products.index'))
 
 @bp.route('/cart/delete/<index>')
@@ -81,15 +84,12 @@ def cart():
     return render_template('orders/cart.html', products=products, grand_total=grand_total, shipping_fee=shipping_fee, total_payment=total_payment)
 
 @bp.route('/cart/checkout', methods=['GET', 'POST'])
-@limiter.limit("1 per minute")
-@cache.cached(timeout=60, key_prefix='checkout')
 @login_required
 def checkout():
     form = CheckoutForm()
     products, grand_total, shipping_fee, total_payment = handle_cart()
-
     if form.validate_on_submit():
-        selected_address_id = int(form.selected_address.data)
+        selected_address_id = form.selected_address.data
         new_order = Orders(
             address_id=selected_address_id,
             buyer_id=current_user.id,
@@ -163,26 +163,80 @@ def checkout():
                 },],
             mode="payment",
             line_items=line_items,
-            success_url=f"{domain_url}/orders/success",
-            cancel_url=f"{domain_url}/orders/cancel",
+            success_url=f"{domain_url}/orders/{order_id}/payment",
+            cancel_url=f"{domain_url}/orders/{order_id}/payment",
         )
 
         order = Orders.query.filter_by(id=order_id).first()
         order.stripe_payment_id = stripe_session.id
+        order.stripe_payment_url = stripe_session.url
         db.session.commit()
 
-        stripe_session_id_url = stripe_session.url
-        db.session.commit()
+        send_checkout_verification(mobile=current_user.mobile, order=order)
 
-        return redirect(stripe_session_id_url, code=303)
+        return redirect(f"/orders/{order_id}")
     return render_template('orders/checkout.html', form=form, products=products, grand_total=grand_total, shipping_fee=shipping_fee, total_payment=total_payment)
 
-@bp.route("/orders/success")
-def payment_successful():
-    flash('Your order has been created successfully.')
+@bp.route("/orders/<uuid:id>/payment")
+def payment_successful(id):
+    order = Orders.query.filter_by(id=id).first()
+    stripe.api_key = os.environ.get('STRIPE_SECRET_KEY_DEV')
+    session = stripe.checkout.Session.retrieve(order.stripe_payment_id)
+    if session.payment_status == "paid":
+        order.payment_status = "paid"
+        db.session.commit()
+        flash('Payment complete')
+    else:
+        flash('Your payment was unsuccessful. Please try again.')
     return redirect(url_for('accounts.settings'))
 
-@bp.route("/orders/cancel")
-def payment_unsuccessful():
-    flash('Your payment was unsuccessful. Please go to our Acount > Order History > Order to find the unique payment link.')
+@bp.route("/orders/<uuid:id>", methods=['GET', 'POST'])
+def order_detail(id):
+    order = Orders.query.filter_by(id=id).first()
+    order_lines = OrderLine.query.filter_by(order_id=id).all()
+    address = DeliveryAddress.query.filter_by(id=order.address_id).first()
+    expiry_time = order.time_created + timedelta(hours=12)
+    remaining_time = expiry_time - datetime.now(pytz.utc)
+    form = VerifyOrderForm()
+    for order_line in order_lines:
+        product = Product.query.filter_by(id=order_line.product_id).first()
+        order_line.product = product
+
+    if request.method=='POST' and form.validate_on_submit():
+        if form.verification_token.data == order.checkout_verification_code:
+            order.is_purchase_verified = True
+            db.session.commit()
+        else:
+            order.checkout_verification_attemps += 1
+            db.session.commit()
+            flash('Your verification code is invalid. Please try again.')
+
+        if order.checkout_verification_attemps > 4:
+            order.delivery_status = "cancelled"
+            order.payment_status = "cancelled"
+            order.is_expired = True
+            db.session.commit()
+
+    return render_template('orders/detail.html',
+                           order=order,
+                           order_lines=order_lines,
+                           title="Order Detail",
+                           address=address,
+                           remaining_time=remaining_time,
+                           form=form)
+
+@bp.route("/orders/<uuid:id>/cancel")
+def cancel_order(id):
+    order = Orders.query.filter_by(id=id).first()
+    order.delivery_status = "cancelled"
+    order.payment_status = "cancelled"
+    order.time_updated = datetime.now(pytz.utc)
+    db.session.commit()
+    flash('Your order has been cancelled successfully.')
+    if order.delivery_status == "cancelled":
+        orderlines = OrderLine.query.filter_by(order_id=id).all()
+        for orderline in orderlines:
+            product = Product.query.filter_by(id=orderline.product_id).first()
+            product.stock += orderline.quantity
+            db.session.commit()
     return redirect(url_for('accounts.settings'))
