e4b15127addb34abb8857803cc1412e499e79eca
kayesokua <kaye.sokua@code.berlin>
2023-04-27 17:23:00 +0200

    fixed model relationships

--- application/models/orders.py
+++ application/models/orders.py
@@ -8,8 +8,8 @@ from application import db
 class Orders(db.Model):
     __tablename__ = 'orders'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    address_id = Column(UUID, ForeignKey('addresses.id'), nullable=False)
-    buyer_id = Column(UUID, ForeignKey('users.id'), nullable=False)
+    address_id = Column(UUID(as_uuid=True), ForeignKey('addresses.id'), nullable=False)
+    buyer_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
     shipping_fee = Column(Float, nullable=False)
     grand_total = Column(Float, nullable=False)
     delivery_status = Column(String(10), default='pending', nullable=True)
@@ -18,19 +18,21 @@ class Orders(db.Model):
     stripe_payment_id = Column(String(255), nullable=True)
     stripe_payment_url = Column(String(350), nullable=True)
     checkout_verification_code = Column(String(6), nullable=True)
-    checkout_verification_attemps = Column(Integer, default=0)
+    checkout_verification_attempts = Column(Integer, default=0)
     is_purchase_verified = Column(Boolean, default=False)
     is_expired = Column(Boolean, default=False)
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
+
     buyer = relationship('User', backref='orders')
     address = relationship('DeliveryAddress', foreign_keys=[address_id], backref='orders')
-    order_lines = relationship('OrderLine', backref='order')
+    order_lines = relationship('OrderLine', backref='order', cascade='all, delete-orphan')
 
 class OrderLine(db.Model):
     __tablename__ = 'order_line'
     order_id = Column(UUID(as_uuid=True), ForeignKey('orders.id'), primary_key=True)
-    product_id = Column(UUID(as_uuid=True), ForeignKey('products.id'), primary_key=True)
+    product_id = Column(UUID(as_uuid=True), ForeignKey('products.id'), nullable=False)
     quantity = Column(Integer, nullable=False)
     total_price = Column(Float, nullable=False)
-    product = relationship('Product')
+
+    product = relationship('Product', lazy='joined', innerjoin=True, foreign_keys=[product_id])
